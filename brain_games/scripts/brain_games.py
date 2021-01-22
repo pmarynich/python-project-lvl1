574c3dd9ee860b1dc80c30376a7b5fd556fb7d3b
pmarynich <marynichp@gmail.com>
2021-01-22 09:30:50 +0300

    $M

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from brain_games.cli import welcome_user
 
+
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()
+    greet = welcome_user()
+    print(greet)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-    
